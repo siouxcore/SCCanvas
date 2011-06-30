331cd67b4afa39ecd61abc5749df71f834859411
nicolas mondon <siouxcore@gmail.com>
2011-06-30 23:20:16 +0200

    version : 0.1.20110630.2
    /**
    * version : 0.1.20110630.2
    * change : render function : created
    *          draw function : created
    *          numrend var : created
    */

--- sccanvas.js
+++ sccanvas.js
@@ -1,5 +1,8 @@
 /**
-*
+* version : 0.1.20110630.2
+* change : render function : created
+*          draw function : created
+*          numrend var : created
 */
 (function (){
   //Creating a framework named SCCanvas
@@ -8,10 +11,14 @@
     canvasid: {},
     context: {},
     framerate: 0,
-    elements: [],
+    elements: {},
+    numrend: 0,
     /**
     * init function
-    * @param : param
+    * @id : id of the canvas (string)
+    * @width : width of the canvas (number)
+    * @height : height of the canvas (number)
+    * @framerate : framerate of the sketch (number)
     **/
     init: function(param){
       // create the canvas
@@ -39,16 +46,65 @@
       }
       // chaining pattern, return this
       return this;
+    },
+    /**
+    * adding element(s) in the renderer queue
+    * @elt : elements (array or object)
+    *        an object element : 
+    *        {
+    *          z: depth (number) - not necessary (0 by default),
+    *          color: color (object) - not necessary ({r:0, g:0, b:0} by default),
+    *          id: identifier (string) - not necessary (will be set by SCC if not) but usefull if you want to refind your elt later
+    *        };
+    * @render : rendering after adding ? (boolean) - not necessary
+    **/
+    add: function(param){
+      var tab = [];
+      var e = param.elt ? param.elt : {};
+      var r = (param.render && typeof param.render === 'boolean') ? param.render : false;
+      Object.prototype.toString.apply(e) === '[object Array]' ? tab = e : tab.push(e);
+      for(ind in tab){
+        var newE = tab[ ind ];
+        if(!newE.z || newE.z === undefined || newE.z == null) newE.z = 0; // z        
+        if(!newE.color || newE.color === undefined || newE.color == null) newE.color = {r:0, g:0, b:0}; // default color
+        if(!newE.id || typeof newE.id !== 'string') { // id
+          var dateTemp = new Date();
+          newE.id = dateTemp.getTime().toString();
+        }
+        this.elements[ newE.id ] = newE;
+      }
+      return (r ? this.render() : this);
+    },
+    /**
+    * render function
+    **/
+    render: function(param){
+      return this;
+    },
+    /**
+    * drawing elements method
+    * @param : no param
+    **/
+    draw: function(){
+      this.numrend++;
+      // clean matrix
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // z sort
+      var sortedElements = [];
+      for(var id in this.elements){
+        sortedElements.push(this.elements[ id ]);
+      }
+      sortedElements.sort(function(a,b){ return a.z - b.z; });
+      // draw
+      for(var sortedId in sortedElements){
+        if(sortedElements[ sortedId ].draw && typeof sortedElements[ sortedId ].draw === 'function'){
+          sortedElements[ sortedId ].draw( this.context );
+        }
+      }
+      // chaining pattern return
+      return this;
     }
   };
-
   // shortcut
   if(!window.SCC){window.SCC = SCCanvas;}
-
 })();
-/*SCC.init({
-  id: "canvas-test",
-  width: 600,
-  height: 400,
-  framerate: 30
-});*/
